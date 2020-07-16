49b9557904342c0b671a5e4fc7bd1b6b171bac2c
Sergey <jlamep7@gmail.com>
2020-07-16 15:57:51 +0300

    Initial Commit

---
+++ SergeyCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SergeyCard
+//
+//  Created by Sergey Luk on 7/16/20.
+//  Copyright © 2020 Sergey Luk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
